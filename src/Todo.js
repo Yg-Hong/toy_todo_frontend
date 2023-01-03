9b1e99e491185601416d167dcb576e8a35a9bddb
Yg-Hong <lion0077v@gmail.com>
2023-01-03 14:48:59 +0900

    [update]

--- src/Todo.js
+++ src/Todo.js
@@ -1,70 +1,74 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import {
-    ListItem,
-    ListItemText,
-    InputBase,
-    Checkbox,
-    ListItemSecondaryAction,
-    IconButton,
-    }  from "@mui/material";
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@mui/material";
 import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
 
 const Todo = (props) => {
-    const [item, setItem] = useState(props.item);
-    const [readOnly, setReadOnly] = useState(true);
-    const deleteItem = props.deleteItem;
-    const editItem = props.editItem;
+  const [item, setItem] = useState(props.item);
+  const [readOnly, setReadOnly] = useState(true);
+  const deleteItem = props.deleteItem;
+  const editItem = props.editItem;
 
-    const deleteEventHandelr = () => {
-        deleteItem(item);
-    };
+  const editEventHandler = (e) => {
+    setItem({...item, title: e.target.value});
+  };
 
-    const editEventHandler = (e) => {
-        setItem({...item, title: e.target.value});
-    };
+  const checkboxEventHandler = (e) => {
+    item.done = e.target.checked;
+    editItem(item);
+  }
 
-    const checkboxEventHandler = (e) => {
-        editItem({...item, title: e.target.checked});
+  // deleteEventHandler 작성
+  const deleteEventHandler = () => {
+    deleteItem(item);
+  };
+
+  const turnOffReadOnly = () => {
+    setReadOnly(false);
+  }
+
+  // turnOnReadOnly 함수 작성
+  const turnOnReadOnly = (e) => {
+    if (e.key === "Enter" && readOnly === false) {
+        setReadOnly(true);
+        editItem(item);
     }
+  };
 
-    const turnOffReadOnly = () => {
-            setReadOnly(false);
-    };
-
-    const turnOnReadOnly = (e) => {
-        if(e.key === "Enter" && readOnly === false){
-            setReadOnly(true);
-            editItem(item);
-        }
-    };
-
-    return(
-        <ListItem>
-            <Checkbox checked = {item.done} onChange={checkboxEventHandler} />
-            <ListItemText>
-                <InputBase
-                    inputProps={{
-                        "aria-label": "naked",
-                        readOnly: readOnly }}
-                    onClick={turnOffReadOnly}
-                    onKeyDown={turnOnReadOnly}
-                    onChange={editEventHandler}
-                    type={"text"}
-                    id={item.id}
-                    name={item.id}
-                    value={item.title}
-                    multiline={true}
-                    fullwidth={true}
-                    />
-            </ListItemText>
-            <ListItemSecondaryAction>
-                <IconButton aria-label={"Delete Todo"}
-                    onClick={deleteEventHandelr} >
-                    <DeleteOutlined />
-                </IconButton>
-            </ListItemSecondaryAction>
-        </ListItem>
-    );
+  return (
+    <ListItem>
+      <Checkbox checked={item.done}
+      onChange={checkboxEventHandler} />
+      <ListItemText>
+        <InputBase
+          inputProps={{ 
+              "aria-label": "naked",
+              readOnly: readOnly }}
+          onClick={turnOffReadOnly}
+          onKeyDown={turnOnReadOnly}
+          onChange={editEventHandler}
+          type="text"
+          id={item.id}
+          name={item.id}
+          value={item.title}
+          multiline={true}
+          fullWidth={true}
+        />
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete Todo"
+          onClick={deleteEventHandler} >
+          <DeleteOutlined />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
 };
 
 export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+import AppRouter from './AppRouter';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const container = document.getElementById('root');
+
+const root = createRoot(container); 
+
+root.render(<AppRouter tab="home" />);
+
 reportWebVitals();
